07c1dba7040e58fc9cabfcd0c5eb4fbc7dbd3c96
MujahidRizvi <superizvi@gmail.com>
2022-09-26 18:31:40 +0500

    go1

--- utils/enums.ts
+++
@@ -1,8 +0,0 @@
-export const enum Provider {
-  // these values are in lower case for consistency
-  Wallet = 'wallet',
-  Google = 'google',
-  Microsoft = 'microsoft',
-  Facebook = 'facebook',
-  Apple = 'apple',
-}
